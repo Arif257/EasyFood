12d5e07fa84cd00594cef38f0bcdd672f14bf72f
Arif <arifemran018@gmail.com>
2022-07-05 17:30:44 +0600

    EasyFood details update

--- app/src/main/java/com/example/easyfood/activities/MealActivity.kt
+++ app/src/main/java/com/example/easyfood/activities/MealActivity.kt
@@ -3,24 +3,46 @@ package com.example.easyfood.activities
 import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import com.bumptech.glide.Glide
 import com.example.easyfood.R
 import com.example.easyfood.databinding.ActivityMealBinding
 import com.example.easyfood.fragments.HomeFragment
+import com.example.easyfood.pojo.Meal
+import com.example.easyfood.viewModel.MealViewModel
 
 class MealActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMealBinding
     private lateinit var mealId:String
     private lateinit var mealName:String
     private lateinit var mealThumb:String
+    private lateinit var mealMvvm: MealViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMealBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        mealMvvm = ViewModelProviders.of(this)[MealViewModel::class.java]
 
         getMealInformationFromIntent()
         setInformationView()
+
+        mealMvvm.getMealDetail(mealId)
+        observerMealDetailsLiveData()
+    }
+
+    private fun observerMealDetailsLiveData() {
+        mealMvvm.observerMealDetailsLiveData().observe(this,object : Observer<Meal>{
+            override fun onChanged(t: Meal?) {
+                val meal = t
+                binding.tvCategory.text = "Category : ${meal!!.strCategory}"
+                binding.tvArea.text = "Area : ${meal.strArea}"
+                binding.tvInstructionSteps.text = meal.strInstructions
+            }
+
+        })
     }
 
     private fun setInformationView() {
@@ -29,6 +51,7 @@ class MealActivity : AppCompatActivity() {
             .into(binding.imgMealDetail)
         binding.collapsingToolbar.title = mealName
         binding.collapsingToolbar.setCollapsedTitleTextColor(resources.getColor(R.color.white))
+        binding.collapsingToolbar.setExpandedTitleColor(resources.getColor(R.color.purple_200))
     }
 
     private fun getMealInformationFromIntent() {
